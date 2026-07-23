fdb699012d5ae8e2218e48b06e4ed78c7a30e7a0
4gs6776-lang <4gs6776@gmail.com>
2026-07-23 17:50:45 +0100

    Update pharmacyRoutes.js

--- src/routes/pharmacyRoutes.js
+++ src/routes/pharmacyRoutes.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const router = express.Router();
+const { getDrugs, addDrug, dispenseDrug } = require('../controllers/pharmacyController');
 
+// @route   GET /api/pharmacy
+// @desc    Get all drugs
+router.get('/', getDrugs);
+
+// @route   POST /api/pharmacy
+// @desc    Add new drug
+router.post('/', addDrug);
+
+// @route   PUT /api/pharmacy/:id/dispense
+// @desc    Dispense drug
+router.put('/:id/dispense', dispenseDrug);
+
+module.exports = router;
