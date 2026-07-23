e42544c69fa489cb33a2672a2560cc1919d56ccc
4gs6776-lang <4gs6776@gmail.com>
2026-07-23 15:13:29 +0100

    Update hospitalRoutes.js

--- src/routes/hospitalRoutes.js
+++ src/routes/hospitalRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { getHospitals, addHospital } = require('../controllers/hospitalController');
 
+// @route   GET /api/hospitals
+// @desc    Get all hospitals
+router.get('/', getHospitals);
+
+// @route   POST /api/hospitals
+// @desc    Add a new hospital
+router.post('/', addHospital);
+
+module.exports = router;
