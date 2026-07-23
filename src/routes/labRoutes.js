4a0b49c5f3571dd7a0b3be6e761121090979ace9
4gs6776-lang <4gs6776@gmail.com>
2026-07-23 17:36:59 +0100

    Update labRoutes.js

--- src/routes/labRoutes.js
+++ src/routes/labRoutes.js
@@ -1,1 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const { getLabTests, updateLabResult } = require('../controllers/labController');
 
+// @route   GET /api/labs
+// @desc    Get all lab tests
+router.get('/', getLabTests);
+
+// @route   PUT /api/labs/:id
+// @desc    Update lab result
+router.put('/:id', updateLabResult);
+
+module.exports = router;
