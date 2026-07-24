d8b001f7681fc503ce1dfbb4f79e413685d2eebc
4gs6776-lang <4gs6776@gmail.com>
2026-07-24 03:15:48 +0100

    Update drugChartRoutes.js

--- src/routes/drugChartRoutes.js
+++ src/routes/drugChartRoutes.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const router = express.Router();
-const { addChartEntry, getPatientChart } = require('../controllers/drugChartController');
+const { addChartEntry, getPatientChart, updateChartEntry, deleteChartEntry } = require('../controllers/drugChartController');
 
 // @route   POST /api/drugchart
-// @desc    Add new entry to drug chart
+// @desc    Add new entry
 router.post('/', addChartEntry);
 
 // @route   GET /api/drugchart/:patient_id
-// @desc    Get patient drug chart
+// @desc    Get patient drug history
 router.get('/:patient_id', getPatientChart);
+
+// @route   PUT /api/drugchart/:id
+// @desc    Edit entry
+router.put('/:id', updateChartEntry);
+
+// @route   DELETE /api/drugchart/:id
+// @desc    Delete entry
+router.delete('/:id', deleteChartEntry);
 
 module.exports = router;
