9b7a1fa50c43e1035c88fde3a7e6af44c955245b
4gs6776-lang <4gs6776@gmail.com>
2026-07-28 11:45:34 +0100

    Update server.js

--- server.js
+++ server.js
@@ -139,6 +139,7 @@ app.use('/api/hmo', hmoRoutes);
 app.use('/api/reports', reportRoutes);
 app.use('/api/audit', auditRoutes);
 app.use('/api/users', userRoutes);
+app.use('/api/seed', require('./routes/seed'));
 
 // Start server
 const PORT = process.env.PORT || 10000;
