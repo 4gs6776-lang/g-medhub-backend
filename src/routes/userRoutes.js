42c676d09c3b3e684f66bc4f820b80163b1890e5
4gs6776-lang <4gs6776@gmail.com>
2026-07-28 16:05:55 +0100

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { getHospitalStaff, addStaff } = require('../controllers/userController');
+const { getHospitalStaff, addStaff, deleteStaff, resetPassword } = require('../controllers/userController');
 
 // @route   GET /api/users
 // @desc    Get all hospital staff
@@ -9,5 +9,13 @@ router.get('/', getHospitalStaff);
 // @route   POST /api/users
 // @desc    Add a new staff member
 router.post('/', addStaff);
+
+// @route   DELETE /api/users/:id
+// @desc    Delete a staff member
+router.delete('/:id', deleteStaff);
+
+// @route   PUT /api/users/:id/reset-password
+// @desc    Reset staff password
+router.put('/:id/reset-password', resetPassword);
 
 module.exports = router;
